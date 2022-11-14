5cdd88ec170330d357d04404c19470a4883c63ba
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2022-11-14 14:53:16 -0300

    Update and rename ex_2.py to ex02_potencia.py

--- ex02_potencia.py
+++ ex02_potencia.py
@@ -1,4 +1,5 @@
 base = input("Insira uma base: ")
 expoente = input ("Insira um expoente: ")
 resultado = float(base) ** float(expoente)
+#f" (formatted string literals) serve para concatenar informações em strings
 print(f"{base} a potência de {expoente} = {resultado}")
