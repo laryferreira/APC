3e528e75da0d4fc3ea18641682894cb54e347390
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2022-11-17 20:43:57 -0300

    Update ex04_lists.py

--- ex04_lists.py
+++ ex04_lists.py
@@ -1,4 +1,4 @@
 #let's learn how to combine lists:
-items = ['anna', 'matty', 'louise']
+items = ['anna', 'matty', 'louise','marty', 'lucas', 'beatrice']
 total_items = items + ['amanda', 'willy']
 print(total_items)
