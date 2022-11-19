03ce1f823cde4d2a73e55dd3b69c1b1f64d2cc05
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2022-11-19 17:35:13 -0300

    Create ex05_str.py

---
+++ ex05_str.py
@@ -0,0 +1,6 @@
+city = "Sydney"
+numberTrips = 5
+history = "I've been to " + city + " " + str(numberTrips) + " times "
+print(history) # => "I've been to Sydney 5 times"
+
+#This is a reminder that you cannot concatenate other types of variables with strings, such as numeric variables---this would return an error.
