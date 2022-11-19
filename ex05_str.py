73fbff2146d9d09fbff0971fbb75fa4e9bc42199
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2022-11-19 17:37:08 -0300

    Update ex05_str.py

--- ex05_str.py
+++ ex05_str.py
@@ -4,3 +4,6 @@ history = "I've been to " + city + " " + str(numberTrips) + " times "
 print(history) # => "I've been to Sydney 5 times"
 
 #This is a reminder that you cannot concatenate other types of variables with strings, such as numeric variables---this would return an error.
+#You have seen that the operator   +  can have different purposes depending on the types of variables you work with: With numeric types, 
+#it is used to add.
+#With strings, it is used to concatenate.
