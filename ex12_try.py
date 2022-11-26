c1ee156f5ef9c9277125539b26a70d1325db7102
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2022-11-26 11:37:48 -0300

    Create ex12_try.py

---
+++ ex12_try.py
@@ -0,0 +1,13 @@
+#Write an algorithm that reads two floating values (x and y), which should represent the coordinates of a point in a plane. Next, determine which quadrant the point belongs, or if you are at one of the Cartesian axes or the origin (x = y = 0).
+
+
+
+#If the point is at the origin, write the message "Origem".
+
+#If the point is at X axis write "Eixo X", else if the point is at Y axis write "Eixo Y".
+
+#Input
+#The input contains the coordinates of a point.
+
+#Output
+#The output should display the quadrant in which the point is.
