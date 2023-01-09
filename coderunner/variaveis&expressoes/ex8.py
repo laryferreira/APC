f93fc6551c69a0bfc29968dd47531d35dab2f543
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-09 14:31:04 -0300

    Create ex8.py

---
+++ coderunner/variaveis&expressoes/ex8.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+s1 = (input())
+s2 = (input())
+p1 = (a + b) * s1
+p2 = (b + c) * s2
+ptotal = p1 + p2
+total_vezes_ac = (a + c) * ptotal
+print(total_vezes_ac)
