d4da85641b14a8a31324770b3ed0ddfe032ca518
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-09 14:35:36 -0300

    Create ex6.py

---
+++ coderunner/funcoes/ex6.py
@@ -0,0 +1,10 @@
+def estrofe(n):
+    print(f'{n} patinhos')
+    print(f'Foram passear')
+    print(f'Além das montanhas')
+    print(f'Para brincar')
+    print(f'A mamãe gritou')
+    print(f'Quá, quá, quá, quá!')
+    print(f'Mas só {n - 1} patinhos')
+    print(f'Voltaram de lá')
+    print(f'Agora só falta aprender a fazer loop!')
