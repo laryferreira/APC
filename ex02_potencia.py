9517cfed8156e1d6cc853dde38fc4e8487fe85bd
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-09 14:20:48 -0300

    Delete ex02_potencia.py

--- ex02_potencia.py
+++
@@ -1,5 +0,0 @@
-base = input("Insira uma base: ")
-expoente = input ("Insira um expoente: ")
-resultado = float(base) ** float(expoente)
-#f" (formatted string literals) serve para concatenar informações em strings - facilitando a leitura do código
-print(f"{base} a potência de {expoente} = {resultado}")
