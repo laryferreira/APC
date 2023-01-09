2d8f4b5c00edc48c45d42a5e56a7ac89984c22f3
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-09 15:04:04 -0300

    Create 1052_mes.py

---
+++ beecrowd/1052_mes.py
@@ -0,0 +1,25 @@
+mes = int(input())
+if mes == 1:
+    print(f'January')
+elif mes == 2:
+    print(f'February')
+elif mes == 3:
+    print(f'March')
+elif mes == 4:
+    print(f'April')
+elif mes == 5:
+    print(f'May')
+elif mes == 6:
+    print(f'June')
+elif mes == 7:
+    print(f'July')
+elif mes == 8:
+    print(f'August')
+elif mes == 9:
+    print(f'September')
+elif mes == 10:
+    print(f'October')
+elif mes == 11:
+    print(f'November')
+elif mes == 12:
+    print(f'December')
