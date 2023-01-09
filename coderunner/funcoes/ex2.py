402d83ce7918cc8678f8b62f3cf6c9c1ba9fc926
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-09 14:34:24 -0300

    Create ex2.py

---
+++ coderunner/funcoes/ex2.py
@@ -0,0 +1,10 @@
+def fahrenheit(t):
+    fahrenheit = t * 1.8 + 32
+    fahrenheit_print = print(f'Fahrenheit: {fahrenheit}')
+    return fahrenheit_print
+def kelvin(t):
+    kelvin = t + 273.15
+    kelvin_print = print(f'Kelvin: {kelvin}')
+    return kelvin_print
+
+
