37de2b4b90d111ba5bb0d115454f417962c379d2
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-09 15:01:21 -0300

    Create 1049_animal.py

---
+++ beecrowd/1049_animal.py
@@ -0,0 +1,23 @@
+x = str(input())
+y = str(input())
+z = str(input())
+
+if x == "vertebrado" and y == "ave" and z == "carnivoro":
+    print('aguia')
+elif x == "vertebrado" and y == "ave" and z == "onivoro":
+    print('pomba')
+
+elif x == "vertebrado" and y == "mamifero" and z == "onivoro":
+    print('homem')
+elif x == "vertebrado" and y == "mamifero" and z == "herbivoro":
+    print('vaca')
+
+elif x == "invertebrado" and y == "inseto" and z == "hematofago":
+    print('pulga')
+elif x == "invertebrado" and y == "inseto" and z == "herbivoro":
+    print('lagarta')
+
+elif x == "invertebrado" and y == "anelideo" and z == "hematofago":
+    print('sanguessuga')
+elif x == "invertebrado" and y == "anelideo" and z == "onivoro":
+    print('minhoca')
