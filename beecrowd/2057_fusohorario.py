9298db7f143cfce27c358fff700bec12c4c4ef2b
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-09 14:56:25 -0300

    Update 2057_fusohorario.py

--- beecrowd/2057_fusohorario.py
+++ beecrowd/2057_fusohorario.py
@@ -1,1 +1,12 @@
+s, t, f = map(int, input().split())
 
+chegada = s + t + f
+
+if chegada == 24:
+    print(0)
+elif chegada > 24:
+    print(chegada - 24)
+elif chegada < 0:
+    print(chegada + 24)
+else:
+    print(chegada)
