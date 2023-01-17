891f7d6a6a228e40d2ae7c53b42da2cbf5466da7
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-17 15:06:44 -0300

    Update ex1.py

--- coderunner/iteracao/ex1.py
+++ coderunner/iteracao/ex1.py
@@ -1,3 +1,16 @@
+'''Onde está o fim?
+Você recebeu uma lista com todos os colaboradores de uma empresa. A lista contém o nome do colaborador, um por linha. Você não sabe o tamanho da lista, mas sabe que ela acaba quando aparece no lugar do nome a palavra "Fim".
+
+Elabore um programa que conte a quantidade de colaboradores da empresa.
+
+
+A Entrada consiste de:
+um número indefinido de linhas que termina com uma linha com a palavra "Fim";
+cada linha contém a string do nome do colaborador.
+
+A Saída deve apresentar:
+Um número inteiro que é a quantidade de funcionários.'''
+
 cont = 0
 while True:
     id = str(input())
