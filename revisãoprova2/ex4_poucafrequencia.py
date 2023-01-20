5723de599df165b1bca2433752bf1399fdd16880
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-20 10:22:04 -0300

    Update ex4_poucafrequencia.py

--- revisãoprova2/ex4_poucafrequencia.py
+++ revisãoprova2/ex4_poucafrequencia.py
@@ -10,3 +10,47 @@ A segunda linha conterá N nomes de estudantes com até 50 caracteres cada nome,
 A terceira linha conterá N registros de frequência, correspondentes aos respectivos estudantes da linha anterior. Os registros virão separados por um único espaço, e contêm apenas os caracteres ‘A’, ‘P’ e ‘M’. Um ‘P’ indica que o estudante estava presente à aula, ‘A’ indica que ele estava ausente (ele cabulou à aula) e ‘M’ mostra que, apesar de não ir à aula, ele entregou um atestado médico, então esta aula não deverá ser considerada no cálculo da frequência do estudante. Registros de frequência conterão ao menos um caracter ‘A’ ou ‘P’.
 Saída
 Para cada caso de teste imprima os nomes de todos os estudantes que não cumpriram a presença mínima requerida, separados por um espaço. Não deixe espaços sobrando no final da linha. ''''
+
+t = int(input()) #casos de teste
+while t > 0: #entrada de casos de teste
+  n = int(input())
+  estudantes = input()
+  frequencia = input()
+  ncaract_est = len(estudantes) #aqui teremos o número de caracteres de cada entrada pedida
+  ncaracte_freq = len(frequencia)
+  
+  i = 0 #definindo variaveis pra incrementar depois #casosAP
+  total = 0 #totaldealunos
+  resp = '' #string que receberá a resposta final com o nome dos estudantes
+  e = 0 #casoestudantes
+  
+  for j in range(0,n):
+    estudante = ''
+    while e < ncaract_est and estudantes[e] != ' ': #le-se enquanto "e" é menor que numero de caracteres da string estudante e "e" é diferente de espaço vazio:
+      estudante += estudantes[e] #adicionando o valor da string estudante com cada componente da lista estudantes
+      e+=1 #para cada caso, adiciona-se um "e"
+      
+    A: 0 #variáveis estilo contador para cada caso de ausencia e presenca
+    P: 0
+    
+    while i < ncaracte_freq and frequencia[i] != ' ': #desconsidera o espaço previso
+      if frequencia[i] = A: #analisando e comparando os termos da string
+        A+=1
+      elif frequencia[i] = P:
+        P+=1
+      i+=1
+      #O M não é considerado pois não afeta as frequencias
+    
+    if P/(A+P) < 0.75: #não cumpriu presença mínima
+      resp+=estudante+" " #formatando resposta do nome dos estudantes
+      total +=1
+    
+    e += 1 #estudantes #incrementando no while
+    i += 1 #casos AP 
+  
+  if total>0: #definindo prints (imprimir apenas os estudantes que não cumpriram presença mínima)
+    print(resp[:-1]) #retirar espaço sobrando
+  else:
+    print()
+  
+  t-=1 #loop 
