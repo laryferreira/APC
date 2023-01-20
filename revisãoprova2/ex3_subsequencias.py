fcbe0b07f7b7c6f4af5295c8487039801ba400e3
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-20 10:01:17 -0300

    Update ex3_subsequencias.py

--- revisãoprova2/ex3_subsequencias.py
+++ revisãoprova2/ex3_subsequencias.py
@@ -11,40 +11,40 @@ ou
 Caso #2:
 Nao existe subsequencia
 '''''
-ncasos = 1
+ncasos = 1 #iniciamos com caso 1 e não 0
 while True:
-    try:
+    try: #try usado para depois utilizarmos except EOFError: break
         string_1 = input()
         string_2 = input()
 
-        n1 = len(string_1)
+        n1 = len(string_1) #retornando o numero de caracteres de uma string
         n2 = len(string_2)
 
-        qnt_sub = 0
-        pos = 0
+        qnt_sub = 0 #quantidade de substrings
+        pos = 0 #posição
 
-        for i in range(0, n2):
-            y = i
+        for i in range(0, n2): #range de 0 até o tamanho da string 
             tam = 0
-            x = 0
+            y = i #definindo variáveis pra incrementar posteriormente 
+            x = 0 # y caractere na string 2, x caractere na string 1
 
-            while x < n1 and y < n2 and string_1[x] == string_2[y]:
-                tam += 1
+            while x < n1 and y < n2 and string_1[x] == string_2[y]: #le-se enquanto x menor que numero de caracteres da string 1 e y menor que numero de caracteres da string 2 e o x caractere de string 1 = y caractere de string 2
+                tam += 1 
                 x += 1
-                y += 1
+                y += 1 #aqui está percorrendo as strings
 
-            if tam == n1:
-                qnt_sub += 1
-                pos = y - tam
+            if tam == n1: #le-se se tamanho da string é igual a substring ("na mesma ordem e de forma contígua")
+                qnt_sub += 1 #aumenta o numero de substrings
+                pos = y - tam #posicao = caractere na string - tamanho da substring
 
-        print(f'Caso #{ncasos}:')
+        print(f'Caso #{ncasos}:') #formatação do output
         if qnt_sub > 0:
             print(f'Qtd.Subsequencias: {qnt_sub}')
-            print(f'Pos: {pos+1}\n')
+            print(f'Pos: {pos+1}\n') #aqui eu realmente não sei pq adicionou 1, quem souber coloca um pull request aí!!
         else:
-            print('Nao existe subsequencia\n')
-
-        ncasos += 1
+            print('Nao existe subsequencia\n') #\n serve pra pular a linha
+#ps: Não se esqueça que o print tem que estar fora do for, senão ele imprimirá a cada caso e não no último caso apenas
+        ncasos += 1 #numero de casos final
 
     except EOFError:
-        break
+        break #finalizando o try 
