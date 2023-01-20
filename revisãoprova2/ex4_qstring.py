dd611698632b38e8b3f2ca0d2bd1a868bf424f46
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-01-20 11:47:34 -0300

    Update ex4_qstring.py

--- revisãoprova2/ex4_qstring.py
+++ revisãoprova2/ex4_qstring.py
@@ -1,2 +1,10 @@
 ''''print(s[::2]) # Imprime os caracteres nos índices pares
 print(s[1::2]) # Imprime os caracteres nos índices ímpares''''
+
+frase = input()
+frase_sblank = frase
+frase_sblank = frase_sblank[1::2]
+
+frase_sblank = frase_sblank.replace(" ", "")
+
+print(frase_sblank)
