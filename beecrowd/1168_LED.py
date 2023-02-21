0a960b0b155af56b7f4a4dbe737bcdca12cde825
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-02-21 10:10:22 -0300

    Create 1168_LED.py

---
+++ beecrowd/1168_LED.py
@@ -0,0 +1,14 @@
+(https://www.beecrowd.com.br/judge/pt/problems/view/1168)
+
+dic = {'0':6,'1':2,'2':5,'3':5,'4':4,'5':5,'6':6,'7':3,'8':7,'9':6}
+
+n = int(input())
+
+while n > 0:
+    string = input()
+    ans = 0
+    for c in string:
+        ans+= dic[c]
+
+    print(f'{ans} leds')
+    n -=1
